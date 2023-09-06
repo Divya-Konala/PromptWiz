6cb78a730561a2fe732a5480bac5ce6c8b0069a0
Divya-Konala <87460968+Divya-Konala@users.noreply.github.com>
2023-09-06 21:43:46 +0530

    home page

---
+++ app/page.jsx
@@ -0,0 +1,18 @@
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        {/* <br className="max-md:hidden" /> */}
+        <br/>
+        <span className="orange_gradient"> AI Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        PromptWiz is an open-source AI prompt tool for modern world to discover,
+        create & share creative prompts
+      </p>
+    </section>
+  );
+};
+
+export default Home;
